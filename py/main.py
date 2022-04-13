898a03920f1255c2807e440960452340828c2878
Jonas Goos <jonasgoos@goos.de>
2022-04-13 11:13:55 +0200

    Add OpenFace code and sample images

--- py/main.py
+++ py/main.py
@@ -1,23 +1,29 @@
 import time
 import zmq
 import base64
+import json
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.bind("tcp://127.0.0.1:5555")
 
-image = open("image.jpg", 'rb')
-bytes = bytearray(image.read())
-strng = base64.b64encode(bytes)
 
-for request in range(1):
-    
+
+for request in range(6):
+    image = open("sample" + str(request) + ".jpg", 'rb')
+    bytes = bytearray(image.read())
+    strng = base64.b64encode(bytes)
+
     # print(f"Sending request {strng} ...")
     socket.send(strng)
 
     #  Get the reply.
     message = socket.recv()
-    print(f"Received reply {request} [ {message} ]")
+    # print(f"Received reply {request} [ {message} ]")
+
+    data = json.loads(message)
+
+    print(data["presence"]["AU01"])
 
 image.close()
     
